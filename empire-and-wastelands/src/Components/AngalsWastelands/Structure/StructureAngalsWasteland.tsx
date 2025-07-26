a939fdf67aad240f0a4f26563b7b7e7717bef5aa
Ptomainchik <fantasticdenis@mail.ru>
2025-07-26 22:56:11 +0300

    Structures

--- empire-and-wastelands/src/Components/AngalsWastelands/Structure/StructureAngalsWasteland.tsx
+++ empire-and-wastelands/src/Components/AngalsWastelands/Structure/StructureAngalsWasteland.tsx
@@ -1,11 +1,79 @@
 import classes from "../../Styles/Structures.module.css";
 import { BackButton } from "../../Buttons/BackButton";
 import { HomeButton } from "../../Buttons/HomeButton";
+import { useState } from "react";
+import imageRegion from "../../../Images/AngalsWastelandsImage/WastelandBackgruondChoice.jpg";
 
 export const StructureAngalsWasteland = () => {
+    const [showRegionModal, setShowRegionModal] = useState(null);
+
+    function handleOpenModalRegion(id:any) {
+        setShowRegionModal(id);
+    }
+
+    function handleCloseModalRegion() {
+        setShowRegionModal(null);
+    }
+
     return (
-        <div className={classes.structurePage}>
+        <div className={classes.structurePageWasteland}>
             <div className={classes.topbar} style={{background: "darkolivegreen"}}><HomeButton/>Структура<BackButton/></div>
+            
+            <div className={classes.blockButtonStructure}>
+                <button className={classes.region1Button} id={"1"} onClick={() => {handleOpenModalRegion("1")}}>Region1</button>
+                <button className={classes.region2Button} id={"2"} onClick={() => {handleOpenModalRegion("2")}}>Region2</button>
+                <button className={classes.region3Button} id={"3"} onClick={() => {handleOpenModalRegion("3")}}>Region3</button>
+            </div>
+
+            <button className={classes.regionMotherButton} id={"4"} onClick={() => {handleOpenModalRegion("4")}}>RegionMother</button>
+
+            {showRegionModal === "1" &&
+            <div className={classes.regionModal}>
+                <img className={classes.imageRegion} src={imageRegion} alt="images" style={{border: "solid 0.3vw darkolivegreen"}} draggable={false}/>
+                <h3>Region1</h3>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                    In accusantium, odit quae quibusdam adipisci quo soluta sed voluptas, sequi non, debitis voluptatum tempora unde earum rem deserunt laudantium sapiente. 
+                    Consectetur!
+                </p>
+                <button className={classes.closeButton} onClick={handleCloseModalRegion}>Закрыть</button>
+            </div> }
+
+            {showRegionModal === "2" &&
+            <div className={classes.regionModal}>
+                <img className={classes.imageRegion} src={imageRegion} alt="images" style={{border: "solid 0.3vw darkolivegreen"}} draggable={false}/>
+                <h3>Region2</h3>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                    In accusantium, odit quae quibusdam adipisci quo soluta sed voluptas, sequi non, debitis voluptatum tempora unde earum rem deserunt laudantium sapiente. 
+                    Consectetur!
+                </p>
+                <button className={classes.closeButton} onClick={handleCloseModalRegion}>Закрыть</button>
+            </div> }
+
+            {showRegionModal === "3" &&
+            <div className={classes.regionModal}>
+                <img className={classes.imageRegion} src={imageRegion} alt="images" style={{border: "solid 0.3vw darkolivegreen"}} draggable={false}/>
+                <h3>Region3</h3>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                    In accusantium, odit quae quibusdam adipisci quo soluta sed voluptas, sequi non, debitis voluptatum tempora unde earum rem deserunt laudantium sapiente. 
+                    Consectetur!
+                </p>
+                <button className={classes.closeButton} onClick={handleCloseModalRegion}>Закрыть</button>
+            </div> }
+
+            {showRegionModal === "4" &&
+            <div className={classes.regionMotherModal}>
+                <img className={classes.imageRegion} src={imageRegion} alt="images" style={{border: "solid 0.3vw darkolivegreen"}} draggable={false}/>
+                <h3>Region4</h3>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                    In accusantium, odit quae quibusdam adipisci quo soluta sed voluptas, sequi non, debitis voluptatum tempora unde earum rem deserunt laudantium sapiente. 
+                    Consectetur!
+                </p>
+                <button className={classes.closeButton} onClick={handleCloseModalRegion}>Закрыть</button>
+            </div> }
             
         </div>
     )
--- empire-and-wastelands/src/Components/EmpireMarah/Structure/StructureEmpireMarah.tsx
+++ empire-and-wastelands/src/Components/EmpireMarah/Structure/StructureEmpireMarah.tsx
@@ -1,12 +1,52 @@
 import classes from "../../Styles/Structures.module.css";
 import { BackButton } from "../../Buttons/BackButton";
 import { HomeButton } from "../../Buttons/HomeButton";
+import { useState } from "react";
+import imageKingdom from "../../../Images/ImpireMarahImage/EmpireBackgroundChoice.jpg";
 
 export const StructureEmpireMarah = () => {
+    const [showModalKingdom, setShowModalKingdom] = useState(null);
+
+    function handleOpenModalKingdom(id:any){
+        setShowModalKingdom(id);
+    }
+
+    function handleCloseModalKingdom() {
+        setShowModalKingdom(null);
+    }
+
     return (
-        <div className={classes.historyPage}>
+        <div className={classes.structurePageEmpire}>
             <div className={classes.topbar} style={{background: "cadetblue"}}><HomeButton/>Структура<BackButton/></div>
-            
+           
+            <button className={classes.buttonWestKingdom} id={"1"} onClick={() => {handleOpenModalKingdom("1")}}>West</button>
+
+            <button className={classes.buttonEastKingdom} id={"2"} onClick={() => {handleOpenModalKingdom("2")}}>East</button>
+
+            {showModalKingdom === "1" && 
+                <div className={classes.modalWestKingdom} id={"1"}>
+                    <img className={classes.imageKingdom} src={imageKingdom} alt="images" style={{border: "solid 0.3vw darkolivegreen"}} draggable={false}/>
+                    <h3>West Kingdom</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                        Magnam quis animi in deserunt molestias repellendus enim consequatur sequi doloremque eius! 
+                        Unde magnam, ratione qui fuga eius quo hic eos voluptatibus!
+                    </p>
+                    <button className={classes.closeButton} onClick={handleCloseModalKingdom}>Закрыть</button>
+            </div>}
+
+            {showModalKingdom === "2" && 
+                <div className={classes.modalEastKingdom} id={"2"}>
+                    <img className={classes.imageKingdom} src={imageKingdom} alt="images" style={{border: "solid 0.3vw darkolivegreen"}} draggable={false}/>
+                    <h3>East Kingdom</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                        Magnam quis animi in deserunt molestias repellendus enim consequatur sequi doloremque eius! 
+                        Unde magnam, ratione qui fuga eius quo hic eos voluptatibus!
+                    </p>
+                    <button className={classes.closeButton} onClick={handleCloseModalKingdom}>Закрыть</button>
+            </div>}
+
         </div>
     )
 }
