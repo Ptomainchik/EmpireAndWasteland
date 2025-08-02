1305024424d2c388b26aad5f5067d64a4f29f141
Ptomainchik <fantasticdenis@mail.ru>
2025-08-02 07:53:15 +0300

    StructureWastelandImage

--- empire-and-wastelands/src/Components/AngalsWastelands/Structure/StructureAngalsWasteland.tsx
+++ empire-and-wastelands/src/Components/AngalsWastelands/Structure/StructureAngalsWasteland.tsx
@@ -3,6 +3,10 @@ import { BackButton } from "../../Buttons/BackButton";
 import { HomeButton } from "../../Buttons/HomeButton";
 import { useState } from "react";
 import imageRegion from "../../../Images/AngalsWastelandsImage/WastelandBackgruondChoice.jpg";
+import RegionMother from "../../../Images/AngalsWastelandsImage/RegionMother.jpg";
+import CentralWastelands from "../../../Images/AngalsWastelandsImage/CentralWastelands.jpg";
+import InlandWastelands from "../../../Images/AngalsWastelandsImage/InlandWastelands.jpg";
+import OuterWastelands from "../../../Images/AngalsWastelandsImage/OuterWastelands.jpg";
 
 export const StructureAngalsWasteland = () => {
     const [showRegionModal, setShowRegionModal] = useState(null);
@@ -20,17 +24,17 @@ export const StructureAngalsWasteland = () => {
             <div className={classes.topbar} style={{background: "darkolivegreen"}}><HomeButton/>Структура<BackButton/></div>
             
             <div className={classes.blockButtonStructure}>
-                <button className={classes.region1Button} id={"1"} onClick={() => {handleOpenModalRegion("1")}}>Region1</button>
-                <button className={classes.region2Button} id={"2"} onClick={() => {handleOpenModalRegion("2")}}>Region2</button>
-                <button className={classes.region3Button} id={"3"} onClick={() => {handleOpenModalRegion("3")}}>Region3</button>
+                <button className={classes.region1Button} id={"1"} onClick={() => {handleOpenModalRegion("1")}}>Внешние пустоши</button>
+                <button className={classes.region2Button} id={"2"} onClick={() => {handleOpenModalRegion("2")}}>Центральные пустоши</button>
+                <button className={classes.region3Button} id={"3"} onClick={() => {handleOpenModalRegion("3")}}>Внутренние пустоши</button>
             </div>
 
-            <button className={classes.regionMotherButton} id={"4"} onClick={() => {handleOpenModalRegion("4")}}>RegionMother</button>
+            <button className={classes.regionMotherButton} id={"4"} onClick={() => {handleOpenModalRegion("4")}}>Материнские пустоши</button>
 
             {showRegionModal === "1" &&
             <div className={classes.regionModal}>
-                <img className={classes.imageRegion} src={imageRegion} alt="images" style={{border: "solid 0.3vw darkolivegreen"}} draggable={false}/>
-                <h3>Region1</h3>
+                <img className={classes.imageRegion} src={OuterWastelands} alt="Outer Wastelands" style={{border: "solid 0.3vw darkolivegreen"}} draggable={false}/>
+                <h3>Внешние пустоши</h3>
                 <p>
                     Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
                     In accusantium, odit quae quibusdam adipisci quo soluta sed voluptas, sequi non, debitis voluptatum tempora unde earum rem deserunt laudantium sapiente. 
@@ -41,8 +45,8 @@ export const StructureAngalsWasteland = () => {
 
             {showRegionModal === "2" &&
             <div className={classes.regionModal}>
-                <img className={classes.imageRegion} src={imageRegion} alt="images" style={{border: "solid 0.3vw darkolivegreen"}} draggable={false}/>
-                <h3>Region2</h3>
+                <img className={classes.imageRegion} src={CentralWastelands} alt="Central Wastelands" style={{border: "solid 0.3vw darkolivegreen"}} draggable={false}/>
+                <h3>Центральные пустоши</h3>
                 <p>
                     Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
                     In accusantium, odit quae quibusdam adipisci quo soluta sed voluptas, sequi non, debitis voluptatum tempora unde earum rem deserunt laudantium sapiente. 
@@ -53,8 +57,8 @@ export const StructureAngalsWasteland = () => {
 
             {showRegionModal === "3" &&
             <div className={classes.regionModal}>
-                <img className={classes.imageRegion} src={imageRegion} alt="images" style={{border: "solid 0.3vw darkolivegreen"}} draggable={false}/>
-                <h3>Region3</h3>
+                <img className={classes.imageRegion} src={InlandWastelands} alt="Inland Wastelands" style={{border: "solid 0.3vw darkolivegreen"}} draggable={false}/>
+                <h3>Внутренние пустоши</h3>
                 <p>
                     Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
                     In accusantium, odit quae quibusdam adipisci quo soluta sed voluptas, sequi non, debitis voluptatum tempora unde earum rem deserunt laudantium sapiente. 
@@ -65,8 +69,8 @@ export const StructureAngalsWasteland = () => {
 
             {showRegionModal === "4" &&
             <div className={classes.regionMotherModal}>
-                <img className={classes.imageRegion} src={imageRegion} alt="images" style={{border: "solid 0.3vw darkolivegreen"}} draggable={false}/>
-                <h3>Region4</h3>
+                <img className={classes.imageRegion} src={RegionMother} alt="Mother Wastelands" style={{border: "solid 0.3vw darkolivegreen"}} draggable={false}/>
+                <h3>Материнские пустоши</h3>
                 <p>
                     Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
                     In accusantium, odit quae quibusdam adipisci quo soluta sed voluptas, sequi non, debitis voluptatum tempora unde earum rem deserunt laudantium sapiente. 
